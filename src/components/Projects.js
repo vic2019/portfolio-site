23ba3f608420b63a38a5d40196eb55ce44d72bc5
Victor <victor.fan.wang@gmail.com>
2019-05-18 21:56:46 -0700

    Added project cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { projectCards } from './ProjectCards';
 
 export default function Projects() {
   return (
     <div id='projects'>
       <h1>Projects</h1>
+      <div className='project-container'>
+        {projectCards}
+      </div>
     </div>
   );
 }
---
+++ src/components/projectCards.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ProjectCard() {
+  return (
+    <div className='project-card'/>
+  )
+}
+
+
+
+export const projectCards = [
+  ProjectCard(),
+  ProjectCard(),
+  ProjectCard()
+];
