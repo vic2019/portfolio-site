de71e5ce051058e8d4b06cd6ea2c76fbb5d68b0a
Victor <victor.fan.wang@gmail.com>
2019-05-25 11:43:44 -0700

    Edited Home

--- src/components/About.js
+++ src/components/About.js
@@ -14,17 +14,17 @@ export default function About() {
         />
       </div>
       <div className='bio-container'>
-        <p>Hello, my name is Victor. I am a self-taught developer (in training) seeking a junior developer position. <a 
+        <p>I am a self-taught developer (in training) seeking a junior developer position. <a 
           href={resume}
           rel='noopener noreferrer' 
           target='_blank' 
           style={{textDecoration: 'none', color: '#3f3fff', fontFamily: ['Calibri', 'Arial']}}
         >(Resume)</a></p>
         <p>Technologies that I’ve worked with—</p>
-        <br/><ul>
+        <ul>
           <li>Front-end: <b>HTML</b>, <b>CSS</b>, <b>Javascript</b>, <b>React</b>, <b>Redux</b>, <b>react-spring</b>, <b>Jest</b></li>
           <li>Back-end: <b>Node.js</b>, <b>MySQL</b>, <b>AWS (Lambda, DynamoDB, CloudFront, Cognito, SES, SNS, etc.)</b></li>
-        </ul><br/>
+        </ul>
         <p>In July 2018, I stumbled upon "<i>Automate the Boring Stuff with Python</i>" by Al Sweigart, and was immediately hooked.</p>
         <p>At the time I was working to become an attorney in California, but as I delved deeper into computer programming, my passion for coding grew until it dawned on me that I must turn this hobby into a career!</p> 
         <p>I love building things, and I’m committed to continually learning and improving the quality of my work to exceed expectations. Please check out my projects!</p>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { useSpring, animated } from 'react-spring';
+import { useGesture } from 'react-use-gesture';
+
 
 export default function Home() {
+  const trigger = window.innerHeight * 0.4;
+  const [{ opacity, bottom, visibility }, set] = useSpring(() => ({ 
+    to: {opacity: 1, bottom: '0vh', visibility: 'visible'},
+    from: {opacity: 0, bottom: '50vh', visibility: 'visible'},
+    config: { friction: 20, mass: 2 },
+    delay: 300
+  }));
+
+  const bind = useGesture(
+    { onScroll: _ => {
+      if (window.pageYOffset <= trigger) {
+        set({
+          to: {opacity: 1, bottom: '0vw', visibility: 'visible'},
+          from: {opacity: 0, bottom: '25vw', visibility: 'visible'}
+        });
+      } else {
+        set({
+          to: [
+            {opacity: 0, bottom: '25vw', visibility: 'visible'},
+            {opacity: 0, bottom: '25vw', visibility: 'hidden'}
+          ],
+          from: {opacity: 1, bottom: '0vh', visibility: 'visible'},
+          config: { clamp: true, friction: 0 }
+        });
+      }
+    }}, 
+    { domTarget: window }
+  );
+
+  useEffect(bind, [bind]);
+  
   return (
-    <div id='home'/>
+    <div id='home'>
+      <animated.div style={{opacity, visibility}} className='greeting'>
+        <animated.p style={{bottom}}>Hello, I'm <span>Victor Wang</span>.<br/>
+        Welcome to my portfolio :)</animated.p>
+      </animated.div>
+    </div>
   );
 }
